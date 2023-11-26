c74be5550be274c419f17d8130c87444e73be348
CastleLong911 <castlelong911@gmail.com>
2023-11-27 03:32:26 +0900

    23/11/27 03:32

--- card.js
+++ card.js
@@ -1,6 +1,7 @@
-function createHTMLElement(data) {
+function createCard(data, deadline, dday) {
+    console.log(data)
     return `
-    <div class="w-full card" topic-id="${data.topicId}">
+    <div class="${dday[1] == '+' ? 'blur-sm ' : ' '}w-full card" topic-id="${data.roomId}" onclick="window.location.href='topicRoom?roomId='+'${data.roomId}'">
 
     <div class="w-full h-64 bg-gray-300 rounded-lg dark:bg-gray-600 flex items-center">
       <div class="w-full h-64 bg-white dark:bg-zinc-800 h-52 rounded-lg">
@@ -9,7 +10,7 @@ function createHTMLElement(data) {
             ${data.topic}
           </h4>
           <div class="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
-            <div class="h-6 bg-blue-600 rounded-full dark:bg-blue-500" style="width: ${(data.pros / (data.pros + data.cons) * 100).toFixed(2)}%;"></div>
+            <div class="h-6 bg-blue-600 rounded-full dark:bg-blue-500" style="width: ${data.pros == 0 ? '0' : (data.pros / (data.pros + data.cons) * 100).toFixed(2)}%;"></div>
           </div>
           <div class="mt-6 mb-3 w-full flex justify-around">
             <div class="flex flex-col items-center">
@@ -24,19 +25,19 @@ function createHTMLElement(data) {
             </div>
             <div class="flex flex-col items-center justify-center">
               <p class="text-2xl font-bold text-navy-700 text-black dark:text-white whitespace-nowrap">
-                ${data.population}위
+                ${data.pros + data.cons}
               </p>
-              <p class="text-sm font-normal text-gray-600 dark:text-white whitespace-nowrap">인기</p>
+              <p class="text-sm font-normal text-gray-600 dark:text-white whitespace-nowrap">투표수</p>
             </div>
           </div>
         </div>
       </div>
     </div>
     <h1 class="w-56 h-6 mt-4 bg-gray-200 rounded-lg dark:bg-zinc-800 dark:text-white">
-      <span class="ml-3 font-bold">${data.date} - ${data.date}</span>
+      <span class="ml-3 font-bold">${data.date} - ${deadline}</span>
     </h1>
     <p class="w-24 h-6 mt-4 bg-gray-200 rounded-lg dark:bg-zinc-800 text-center">
-      <span class="font-bold text-sm dark:text-white">D - ${data.period}</span>
+      <span class="font-bold text-sm dark:text-white">${dday}</span>
     </p>
   </div>
     `;
@@ -44,19 +45,19 @@ function createHTMLElement(data) {
 
 // 생성할 데이터 배열
 const dataItems = [
-    { topicId: 'ABCD', topic: 'topic 1', reply: 1234, pros: 232, cons: 123, population: 2, date: '2023.11.25', period: 15 },
-    { topicId: 'EFGH', topic: 'topic 2', reply: 124, pros: 23, cons: 82, population: 3, date: '2023.11.21', period: 12 },
-    { topicId: 'IJKL', topic: 'topic 3', reply: 12324, pros: 9312, cons: 1243, population: 1, date: '2023.11.23', period: 1 },
+    { topicId: 'ABCD', topic: 'topic 1', reply: 1234, pros: 232, cons: 123, population: 2, date: '2023-11-25', period: 15 },
+    { topicId: 'EFGH', topic: 'topic 2', reply: 124, pros: 0, cons: 82, population: 3, date: '2023-11-21', period: 12 },
+    { topicId: 'IJKL', topic: 'topic 3', reply: 12324, pros: 9312, cons: 1243, population: 1, date: '2023-11-23', period: 1 },
 ];
 
 
 
 // 데이터 배열을 순회하면서 HTML 요소 생성 후 컨테이너에 추가
-const elementContainer = document.getElementById('cardContainer');
+const cardContainer = document.getElementById('cardContainer');
 
 dataItems.forEach(itemData => {
-    const elementHTML = createHTMLElement(itemData);
-    elementContainer.innerHTML += elementHTML;
+    const elementHTML = createCard(itemData, addDays(itemData.date, itemData.period), getDday(addDays(itemData.date, itemData.period)));
+    cardContainer.innerHTML += elementHTML;
 });
 
 let cards = document.querySelectorAll('.card');
@@ -67,3 +68,62 @@ cards.forEach(card => {
         window.location.href = 'topicRoom.html?topic='+topic;
     });
 });
+
+function newCard(topic, period){
+
+}
+
+/*
+function setCards(data) {
+    for (item in data) {
+        const card = createCard(data[item], addDays(data.date, data.period), getDday(addDays(data.date, data.period)));
+        cardContainer.innerHTML += card;
+    }
+}
+
+
+function getRoomInformations() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/information', true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            console.log("success ! !");
+            const data = JSON.parse(xhr.response);
+            setCards(data);
+        } else {
+            console.log("fail ! !");
+        }
+    }
+}
+*/
+
+function addDays(date, period) {
+    var newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + period);
+    console.log(newDate.getFullYear());
+    return newDate.getFullYear() + '-' + (newDate.getMonth() + 1).toString().padStart(2, '0') + '-' + newDate.getDate().toString().padStart(2, '0');
+}
+
+function getDday(date) {
+    // 목표 날짜를 Date 객체로 변환
+    var targetDate = new Date(date);
+    targetDate.setHours(0, 0, 0, 0); // 시간, 분, 초, 밀리초를 0으로 설정
+
+    // 현재 날짜
+    var today = new Date();
+    today.setHours(0, 0, 0, 0); // 시간, 분, 초, 밀리초를 0으로 설정
+
+    // 디데이 계산
+    var diff = targetDate - today;
+    var dDay = diff / (1000 * 60 * 60 * 24);
+
+    // 디데이 문자열 포맷팅
+    if (dDay > 0) {
+        return "D-" + Math.floor(dDay); // 디데이까지 남은 일수
+    } else if (dDay < 0) {
+        return "D+" + Math.abs(Math.ceil(dDay)); // 디데이가 지난 일수
+    } else {
+        return "D-Day"; // 오늘일 경우
+    }
+}
--- message.js
+++ message.js
@@ -1,12 +1,12 @@
 function createOtherMSG(data) {
     return `
-        <div className="flex">
-            <div className="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
-                <div className="text-sm align-text-bottom text-left">
-                    ${data.name}
+        <div class="flex">
+            <div class="bg-gray-300 text-black p-2 rounded-lg max-w-xs">
+                <div class="text-sm align-text-bottom text-left">
+                    ${data.sender}
                 </div>
                     ${data.msg}
-                <div className="text-sm w-full align-text-bottom text-right">
+                <div class="text-sm w-full align-text-bottom text-right">
                     ${data.date}
                 </div>
             </div>
@@ -16,16 +16,110 @@ function createOtherMSG(data) {
 
 function createMyMSG(data) {
     return `
-        <div className="flex justify-end">
-            <div className="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
-                <div className="text-sm align-text-bottom text-right">
-                    ${data.name}
+        <div class="flex justify-end">
+            <div class="bg-blue-200 text-black p-2 rounded-lg max-w-xs">
+                <div class="text-sm align-text-bottom text-right">
+                    ${data.sender}
                 </div>
                     ${data.msg}
-                <div className="text-sm w-full align-text-bottom text-right">
+                <div class="text-sm w-full align-text-bottom text-right">
                     ${data.date}
                 </div>
             </div>
         </div>
     `;
+}
+
+function setMsg(data) {
+    const msg = data.kakaoId == kakaoId ? createMyMSG(data) : createOtherMSG(data);
+    if(data.isPro == 1){
+        const flag = checkProScroll();
+        msgProContainer.innerHTML += msg;
+        if(flag || data.kakaoId == kakaoId){
+            proScrollDown();
+        }
+    }
+    else{
+        const flag = checkConScroll();
+        msgConContainer.innerHTML += msg;
+        if(flag || data.kakaoId == kakaoId){
+            conScrollDown();
+        }
+    }
+}
+
+function prependMsg(data){
+    const msg = data.kakaoId == kakaoId ? createMyMSG(data) : createOtherMSG(data);
+    if(data.isPro == 1){
+        const flag = checkProScroll();
+        msgProContainer.innerHTML = msg + msgProContainer.innerHTML;
+        if(flag){
+            proScrollDown();
+        }
+    }
+    else{
+        const flag = checkConScroll();
+        msgConContainer.innerHTML = msg + msgConContainer.innerHTML;
+        if(flag){
+            conScrollDown();
+        }
+    }
+}
+
+function msgInitXhr(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/msgInitXhr/'+roomId, true);
+    xhr.send();
+        xhr.onload = () => {
+            if (xhr.status == 200) {
+            console.log("success ! !");
+            const data = JSON.parse(xhr.response);
+            for (item in data) {
+                setMsg(data[item]);
+            }
+            scrollDown();
+        } else {
+            console.log("fail ! !");
+        }
+    }
+}
+
+function msgScrollXhr(isPro, count){
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/msgScrollXhr/'+roomId+'/'+isPro+'/'+count, true);
+    xhr.send();
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            console.log("success ! !");
+            const data = JSON.parse(xhr.response);
+            for (item in data) {
+                setMsg(data[item]);
+            }
+        } else {
+            console.log("fail ! !");
+        }
+    }
+}
+function proScrollDown(){
+    const element = document.querySelector('#sectionA');
+    element.scrollTop = element.scrollHeight;;
+}
+
+function checkProScroll() {
+    const container = document.getElementById("#sectionA");
+    const isAtBottom = container.scrollHeight - container.clientHeight <= container.scrollTop + 1;
+
+    return isAtBottom;
+}
+
+function conScrollDown(){
+    const element = document.querySelector('#sectionB');
+    element.scrollTop = element.scrollHeight;;
+}
+
+function checkConScroll() {
+    const container = document.getElementById("#sectionB");
+    const isAtBottom = container.scrollHeight - container.clientHeight <= container.scrollTop + 1;
+
+    return isAtBottom;
 }
